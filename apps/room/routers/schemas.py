f293d032fefc104d8fbca84d955eadc907160b78
Nasser <nasser-silva@hotmail.com>
2026-05-01 01:18:57 -0300

    fix: seat response schema

--- apps/room/routers/schemas.py
+++ apps/room/routers/schemas.py
@@ -13,3 +13,9 @@ class SeatSchema(BaseModel):
     horizontal: str
     vertical: str
     room_id: int
+
+class SeatResponseSchema(SeatSchema):
+    horizontal: str
+    vertical: str
+    room_id: int
+    is_available: bool
