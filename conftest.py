5bba7bebed1e73fd95c0036b839d892252f7fddc
Nasser <nasser-silva@hotmail.com>
2026-05-01 01:19:49 -0300

    test: add conftest

---
+++ conftest.py
@@ -0,0 +1,36 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import StaticPool, create_engine
+from sqlalchemy.orm import Session
+
+from database import get_session
+from main import app
+from apps.room.models.room import table_registry
+
+@pytest.fixture
+def client(session):
+    def get_session_override():
+        return session
+        
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
+
+@pytest.fixture
+def session():
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
+
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+        
+    table_registry.metadata.drop_all(engine)
+        
+    engine.dispose()
